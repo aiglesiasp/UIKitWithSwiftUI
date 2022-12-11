832f0b047f813abd2734a4c58e7ea0ed466e2665
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-12-11 17:23:39 +0100

    inital commit

---
+++ UIKitWithSwiftUI/UIKitWithSwiftUI/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UIKitWithSwiftUI
+//
+//  Created by Aitor Iglesias Pubill on 11/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func openViewController(_ sender: Any) {
+    }
+    
+    @IBAction func openTableViewController(_ sender: Any) {
+    }
+}
+
